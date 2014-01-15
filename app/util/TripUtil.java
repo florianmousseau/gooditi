0a1cb81e10278b29dd6a113de342d68f3c3d03f9
Florian <florian.mousseau@gmail.com>
2014-01-15 15:22:14 +0100

    marker

---
+++ app/util/TripUtil.java
@@ -0,0 +1,62 @@
+package util;
+
+public class TripUtil {
+	
+	public static String numberToLetter(int number) {
+		
+		switch (number) {
+		case 1:
+			return "A";
+		case 2:
+			return "B";
+		case 3:
+			return "C";
+		case 4:
+			return "D";
+		case 5:
+			return "E";
+		case 6:
+			return "F";
+		case 7:
+			return "G";
+		case 8:
+			return "H";
+		case 9:
+			return "J";
+		case 10:
+			return "K";
+		case 11:
+			return "L";
+		case 12:
+			return "M";
+		case 13:
+			return "N";
+		case 14:
+			return "O";
+		case 15:
+			return "P";
+		case 16:
+			return "Q";
+		case 17:
+			return "R";
+		case 18:
+			return "S";
+		case 19:
+			return "T";
+		case 20:
+			return "U";
+		case 21:
+			return "V";
+		case 22:
+			return "W";
+		case 23:
+			return "X";
+		case 24:
+			return "Y";
+		case 25:
+			return "Z";					
+		default:
+			return "";
+		}
+	}
+}
